fdf6ce83205955e92d2ad16611703f82d9503e20
Wisionarium <wisionarium@gmail.com>
2026-09-08 01:32:19 -0300

    Mais respiro entre secoes; linhas de venda brancas

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'equipe-comercial-v12';
+const CACHE = 'equipe-comercial-v13';
 const CORE = ['/', '/index.html', '/css/styles.css', '/js/app.js', '/manifest.webmanifest', '/icons/icon.svg'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(CORE)).then(() => self.skipWaiting()));
